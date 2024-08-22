f64fd35b793e9c0ae0b7652401bfac9591e957ad
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2024-08-22 12:52:43 -0700

    add bee generator output

--- beeJson.js
+++ beeJson.js
@@ -23,20 +23,20 @@ function generateJson() {
         tolerance: form['humid_tol'].value,
         dominant: form['humid_dom'].checked
     }
-    if (form.flower.value != "") {
-        object.default_chromosome['complicated_bees:flower'] = {
-            data: form['flower'].value,
-            dominant: form['flower_dom'].checked
-        }
+    object.default_chromosome['complicated_bees:productivity'] = {
+        data: form['productivity'].value,
+        dominant: form['prod_dom'].checked
     }
     if (form.fertility.value == "") form.fertility.value = 2
     object.default_chromosome['complicated_bees:fertility'] = {
         data: form['fertility'].value,
         dominant: form['fertile_dom'].checked
     }
-    object.default_chromosome['complicated_bees:productivity'] = {
-        data: form['productivity'].value,
-        dominant: form['prod_dom'].checked
+    if (form.flower.value != "") {
+        object.default_chromosome['complicated_bees:flower'] = {
+            data: form['flower'].value,
+            dominant: form['flower_dom'].checked
+        }
     }
     if (form.effect.value != "") {
         object.default_chromosome['complicated_bees:effect'] = {
@@ -45,15 +45,15 @@ function generateJson() {
         }
     }
     object.default_chromosome['complicated_bees:nocturnal'] = {
-        data: form['nocturnal'].value,
+        data: form['nocturnal'].checked,
         dominant: form['nocturnal_dom'].checked
     }
     object.default_chromosome['complicated_bees:cave_dwelling'] = {
-        data: form['cave_dwelling'].value,
+        data: form['cave_dwelling'].checked,
         dominant: form['cave_dom'].checked
     }
     object.default_chromosome['complicated_bees:weatherproof'] = {
-        data: form['weatherproof'].value,
+        data: form['weatherproof'].checked,
         dominant: form['weather_dom'].checked
     }
     console.log(JSON.stringify(object))
